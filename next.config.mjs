96a42231971d2299a0ff9aab4a076daa8edf30fc
darkshadowsb4u <darkshadowsb4u@gmail.com>
2026-09-14 07:33:00 +0530

    feat: add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+    ],
+  },
+  experimental: {
+    // any modern react 19 configs
+  },
+};
+
+export default nextConfig;
