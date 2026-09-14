6508fce3d52866a29aac2c8fcd9b4da5c7e19404
darkshadowsb4u <darkshadowsb4u@gmail.com>
2026-09-14 07:31:35 +0530

    feat: add app/journal/[slug]/page.tsx

---
+++ app/journal/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import EditorialArticlePage, {
+  generateMetadata,
+  generateStaticParams,
+} from "../../editorial/[slug]/page";
+
+export { generateMetadata, generateStaticParams };
+export default EditorialArticlePage;
